49d858dcdc63813ee0fce3717fe619a6b3e780ef
Brandtzz <stargateatlantis@hotmail.dk>
2015-11-05 10:44:15 +0100

    Test rettet

--- MovieStore/MovieStoreCustomerTest/OrderLineTest.cs
+++ MovieStore/MovieStoreCustomerTest/OrderLineTest.cs
@@ -12,25 +12,17 @@ namespace MovieStoreCustomerTest
     public class OrderLineTest
     {
 
-        //[Test]
-        //public void Order_line_properties_set_ok()
-        //{
-        //    OrderLine orderLine = new OrderLine();
-        //    Movie movie = new Movie() { Id = 1, Title = "Buller" };
-        //    orderLine.Movie = movie;
-        //    orderLine.Amount = 10;
+        [Test]
+        public void Order_line_properties_set_ok()
+        {
+            OrderLine orderLine = new OrderLine();
+            Movie movie = new Movie() { Id = 1, Title = "Buller" };
+            orderLine.Movie = movie;
+            orderLine.Amount = 1;
 
-        //    Assert.AreEqual(movie, orderLine.Movie);
-        //    Assert.AreEqual(10, orderLine.Amount);
-        //}
-
-        //[Test]
-        //[ExpectedException(typeof(ArgIterator))]
-        //public void OrderLine_amount_less_then_one_fails_test()
-        //{
-        //    OrderLine orderLine = new OrderLine();
-        //    orderLine.Amount = 0;
-        //}
+            Assert.AreEqual(movie, orderLine.Movie);
+            Assert.AreEqual(1, orderLine.Amount);
+        }
 
 
     }
