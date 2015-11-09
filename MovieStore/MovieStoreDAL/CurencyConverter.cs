34946387b1339477bb7ba8aab13c02a877a3e336
Maricano <kenneth_fallesen@hotmail.com>
2015-11-09 10:32:55 +0100

    TDD på currencyConverter

---
+++ MovieStore/MovieStoreDAL/CurencyConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace MovieStoreDAL
+{
+    public class CurencyConverter
+    {
+        public static double dollar;
+        public static double euro;
+
+      public  CurencyConverter()
+      {
+          euro = 7.46;
+          dollar = 6.93;
+      }
+
+        public static double EuroConverter(Movie movie)
+        {
+            double priceInEruo = euro*movie.Price;
+            return priceInEruo;
+        }
+
+        public static double DollarConverter(Movie movie)
+        {
+            double priceInDollar = dollar*movie.Price;
+            return priceInDollar;
+
+        }
+    }
+}
