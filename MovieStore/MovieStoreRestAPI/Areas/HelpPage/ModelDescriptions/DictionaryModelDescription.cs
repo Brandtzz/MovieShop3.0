b88fa3a98fec2ab3c49bbb8451a9268e9fd6931c
Brandtzz <stargateatlantis@hotmail.dk>
2015-11-11 09:29:40 +0100

    Referencer

---
+++ MovieStore/MovieStoreRestAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MovieStoreRestAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MovieStore/MovieStoreRestAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MovieStoreRestAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
