9c034221a0be90d3390f3c6df6bbf9fe6120ba8f
aceymer <ljb@easv.dk>
2015-11-12 16:20:15 +0100

    clering repo

--- MovieStore/MovieStoreRestAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace MovieStoreRestAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- MovieStore/MovieStoreRestAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MovieStoreRestAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
