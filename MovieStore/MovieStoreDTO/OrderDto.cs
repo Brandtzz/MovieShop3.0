56dd63b5d6475743cc9f824749cc089a76b9809b
FrederikSorensen <fred5277@easv365.dk>
2015-11-17 10:27:10 +0100

    Oprettet Dto'er (igen)

--- MovieStore/MovieStoreDTO/OrderDto.cs
+++ MovieStore/MovieStoreDTO/OrderDto.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MovieStoreDTO
 {
-    class OrderDto
+
+    [DataContract(IsReference = true)]
+    public class OrderDto
     {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public DateTime Date { get; set; }
+        [DataMember]
+        public int CustomerId { get; set; }
     }
 }
